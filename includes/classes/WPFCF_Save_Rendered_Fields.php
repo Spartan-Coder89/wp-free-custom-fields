986b8fa44e9fdc7f9c8d61ac1fec6bcdc1e00eeb
Spartan-Coder89 <sjiloma1389@gmail.com>
2026-08-23 23:07:41 +0800

    Created and added meta data saving on pages screens

--- includes/classes/WPFCF_Save_Rendered_Fields.php
+++ includes/classes/WPFCF_Save_Rendered_Fields.php
@@ -7,28 +7,52 @@ class WPFCF_Save_Rendered_Fields {
 
   function __construct() {
     
-    add_action('save_post', function( $post_id ) {
-      
+    add_action('save_post_page', function( $post_id ) {
+
+      if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) return;
+      if ( wp_is_post_revision($post_id) ) return;
+      if ( ! current_user_can('edit_post', $post_id) ) return;
+
       if (isset( $_POST['wpfcf_rendered_fields'] ) and !empty( $_POST['wpfcf_rendered_fields'] )) {
 
         $wpfcf_rendered_fields = $_POST['wpfcf_rendered_fields'];
-
         foreach ($wpfcf_rendered_fields as $key => $field_group_id) {
 
           $field_group = json_decode( get_post_meta( $field_group_id, 'wpfcf_fields_config', true ) );
-          
           foreach ($field_group as $key => $field) {
+
             $post_meta_value = $this->sanitize_field_value( $field->type, $_POST[$field->name] );
             update_post_meta( $post_id, $field->name, $post_meta_value );
           }
         }
       }
-
     });
+
+
+    add_action('save_post', function( $post_id ) {
+      
+      if ( defined('DOING_AUTOSAVE') && DOING_AUTOSAVE ) return;
+      if ( wp_is_post_revision($post_id) ) return;
+      if ( ! current_user_can('edit_post', $post_id) ) return;
+
+      if (isset( $_POST['wpfcf_rendered_fields'] ) and !empty( $_POST['wpfcf_rendered_fields'] )) {
+
+        $wpfcf_rendered_fields = $_POST['wpfcf_rendered_fields'];
+        foreach ($wpfcf_rendered_fields as $key => $field_group_id) {
+
+          $field_group = json_decode( get_post_meta( $field_group_id, 'wpfcf_fields_config', true ) );
+          foreach ($field_group as $key => $field) {
+
+            $post_meta_value = $this->sanitize_field_value( $field->type, $_POST[$field->name] );
+            update_post_meta( $post_id, $field->name, $post_meta_value );
+          }
+        }
+      }
+    });
+
   }
 
   function sanitize_field_value( $type, $raw_value ) {
-
 
     switch( $type ) {
 
